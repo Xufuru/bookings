c8ac9ca000b815d7d58a8d013fc9ce3fcbfc87a5
Xufuru <martino.tosini@gmail.com>
2022-06-16 10:53:25 +0200

    changes 13

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,14 +1,21 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Form creates a custum form struct, embeds a url.values object
 type Form struct {
 	url.Values
 	Errors errors
+}
+
+// Valid returns true if there are no errors, otherwise false
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
 }
 
 // New initializes a form struct
@@ -19,10 +26,30 @@ func New(data url.Values) *Form {
 	}
 }
 
+// Required checks for required fields
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
+}
+
 // Has checks if form field is in post and not empty
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
+		return false
+	}
+	return true
+}
+
+// MinLenght checks for string minimum lenght
+func (f *Form) MinLenght(field string, lenght int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < lenght {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", lenght))
 		return false
 	}
 	return true
